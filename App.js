6f137851593f857569cd759363d32cc1335a1835
prathik8794 <ME20B030@iittp.ac.in>
2022-11-04 01:32:50 +0530

    added some css

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.set("views", path.join(__dirname, "views"));
 
 const octokit = new Octokit({
-  auth: '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'
+  auth: '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'
 });
 
 app.get('/', function(req, res) {
@@ -26,27 +26,26 @@ app.get('/', function(req, res) {
 app.post('/', async (req,res) => {
     console.log(req.body);
     let tmp=[];
-    let owner = req.body.owner;
-    let repo = req.body.repo;
+    let owner1 = req.body.owner;
+    let repo1 = req.body.repo;
     try {
-      for(let j=1;j<=10;j++){
+      for(let j=1;j<=1;j++){
       const result = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
-          owner: owner,
-          repo: repo,
+          owner: owner1,
+          repo: repo1,
           page:j,
           per_page: 100,
-        });
+      });
         console.log(result.data.length);
         for(let i=0;i<result.data.length;i++){
-            // console.log(result.data[i].title+" "+result.data[i].id);
-            // document.getElementById("root").innerText = result.data[i].title;
+             console.log(result.data[i].title+" "+result.data[i].id);
             tmp.push(result.data[i].title);
         }
     }
     } catch (error) {
       console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
     } 
-    res.render('index',{tmp:tmp});
+    res.render('index',{tmp:tmp,repo1:repo1});
 });
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
